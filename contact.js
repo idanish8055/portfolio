01274182f5c111fa316bc6cc9bd0f77ff522c4db
Mohd Danish <mohddanish@Mohds-MacBook-Air.local>
2026-05-17 19:12:01 +0530

    Added Klaviyo success & fail

--- contact.js
+++ contact.js
@@ -5,7 +5,7 @@
 const contactForm = document.getElementById('contactForm');
 const formStatus = document.querySelector('.form-status');
 
-contactForm.addEventListener('submit', function(e) {
+contactForm.addEventListener('submit', async function(e) {
     e.preventDefault();
 
     const name = document.querySelector('#contactForm [name="name"]').value;
@@ -46,11 +46,52 @@ contactForm.addEventListener('submit', function(e) {
         }
     ]);
 
-    /* =========================
-        SUCCESS UI
-    ========================= */
+    try {
 
-    formStatus.innerHTML = 'Message sent successfully 🚀';
-    formStatus.style.color = '#18f2c2';
-    contactForm.reset();
+        const response = await fetch(
+        'https://a.klaviyo.com/client/profiles/?company_id=TjBuVx',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                data: {
+                    type: 'profile',
+                    attributes: {
+                        email: email,
+                        first_name: name,
+                        properties: {
+                            subject: subject,
+                            message: message,
+                            source: 'Portfolio Website'
+                        }
+                    }
+                }
+            })
+
+        });
+
+        if (response.ok) {
+            formStatus.innerHTML = 'Message sent successfully 🚀';
+            formStatus.style.color = '#18f2c2';
+            contactForm.reset();
+            setTimeout(()=>{
+                formStatus.innerHTML = '';
+            }, 5000);
+        } else {
+            const errorData = await response.json();
+            console.log(errorData);
+            formStatus.innerHTML = 'Something went wrong ❌';
+            setTimeout(()=>{
+                formStatus.innerHTML = '';
+            }, 6000);
+        }
+    } catch (error) {
+        console.log(error);
+        formStatus.innerHTML = 'Network error ❌';
+        setTimeout(()=>{
+            formStatus.innerHTML = '';
+        }, 6000);
+    }
 });
