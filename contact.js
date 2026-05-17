f68d1444d9527bd03948e2dadf978d7f7f5b8ec8
Mohd Danish <mohddanish@Mohds-MacBook-Air.local>
2026-05-17 19:20:31 +0530

    Update Klaviyo form

--- contact.js
+++ contact.js
@@ -13,83 +13,51 @@ contactForm.addEventListener('submit', async function(e) {
     const subject = document.querySelector('#contactForm [name="subject"]').value;
     const message = document.querySelector('#contactForm [name="message"]').value;
 
-    /* =========================
-        IDENTIFY USER
-    ========================= */
-
-    window._learnq = window._learnq || [];
-
-    window._learnq.push([
-        'identify',
-        {
-            $email: email,
-            $first_name: name,
-            subject: subject,
-            message: message,
-            source: 'Portfolio Website'
-        }
-    ]);
-
-    /* =========================
-        TRACK EVENT
-    ========================= */
-
-    window._learnq.push([
-        'track',
-        'Portfolio Contact Form Submitted',
-        {
-        Name: name,
-        Email: email,
-        Subject: subject,
-        Message: message,
-        Source: 'Portfolio Website'
-        }
-    ]);
-
     try {
 
-        const response = await fetch(
-        'https://a.klaviyo.com/client/profiles/?company_id=TjBuVx',
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                data: {
-                    type: 'profile',
-                    attributes: {
-                        email: email,
-                        first_name: name,
-                        properties: {
-                            subject: subject,
-                            message: message,
-                            source: 'Portfolio Website'
-                        }
-                    }
-                }
-            })
+        /* =========================
+            IDENTIFY USER
+        ========================= */
 
-        });
+        window._learnq = window._learnq || [];
 
-        if (response.ok) {
-            formStatus.innerHTML = 'Message sent successfully 🚀';
-            formStatus.style.color = '#18f2c2';
-            contactForm.reset();
-            setTimeout(()=>{
-                formStatus.innerHTML = '';
-            }, 5000);
-        } else {
-            const errorData = await response.json();
-            console.log(errorData);
-            formStatus.innerHTML = 'Something went wrong ❌';
-            setTimeout(()=>{
-                formStatus.innerHTML = '';
-            }, 6000);
-        }
+        window._learnq.push([
+            'identify',
+            {
+                $email: email,
+                $first_name: name,
+                subject: subject,
+                message: message,
+                source: 'Portfolio Website'
+            }
+        ]);
+
+        /* =========================
+            TRACK EVENT
+        ========================= */
+
+        window._learnq.push([
+            'track',
+            'Portfolio Contact Form Submitted',
+            {
+                Name: name,
+                Email: email,
+                Subject: subject,
+                Message: message,
+                Source: 'Portfolio Website'
+            }
+        ]);
+
+        formStatus.innerHTML = 'Message sent successfully 🚀';
+        formStatus.style.color = '#18f2c2';
+        contactForm.reset();
+        
+        setTimeout(()=>{
+            formStatus.innerHTML = '';
+        }, 5000);
     } catch (error) {
         console.log(error);
-        formStatus.innerHTML = 'Network error ❌';
+        formStatus.innerHTML = 'Something went wrong ❌';
         setTimeout(()=>{
             formStatus.innerHTML = '';
         }, 6000);
